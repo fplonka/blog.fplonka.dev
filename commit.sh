eb5f3dcffaa8f85fa6e22be88abf172150a1e141
Filip Płonka <github@1f389.com>
2025-02-09 23:09:03 +0100

    Updating blog content

--- commit.sh
+++ commit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rsync -a --delete "/Users/filip/Library/Mobile Documents/iCloud~md~obsidian/Documents/synced vault/blog.fplonka.dev/" content/
-git add -A && git commit -m "Updating blog content"
+git add -A
+git commit -m "Updating blog content"
+GIT_SSH_COMMAND="ssh -i ~/.ssh/github_script" git push
