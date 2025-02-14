c279fafa7d696c5df9fdc230af008a369c9a5391
Filip Płonka <github@1f389.com>
2025-02-14 11:39:34 +0000

    Update blog content

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 rsync -a --delete "/Users/filip/Library/Mobile Documents/iCloud~md~obsidian/Documents/synced vault/blog.fplonka.dev/" content/
 
 # TODO: compress images maybe
-cp "/Users/filip/Library/Mobile Documents/iCloud~md~obsidian/Documents/synced vault/blog.fplonka.dev/images/*" static/
+cp "/Users/filip/Library/Mobile Documents/iCloud~md~obsidian/Documents/synced vault/blog.fplonka.dev/images"/* static/
 
 git add -A
 git commit -m "Update blog content"
